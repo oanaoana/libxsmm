6016297824af1243f8dcc87b1df865265d5fcc60
Oana Marin <oanam@anl.gov>
2016-11-30 14:11:15 -0600

    streaming stores for nek5000, still using f90 wrapper, no_h2 fixed

--- samples/nek/stream_update_kernels.f
+++ samples/nek/stream_update_kernels.f
@@ -85,6 +85,21 @@ MODULE STREAM_UPDATE_KERNELS
       INTEGER(C_INT),      VALUE,        INTENT(IN)    :: i_length
     END SUBROUTINE
 
+    SUBROUTINE stream_update_var_helmholtz_no_h2( i_g1, i_g2, i_g3, i_tm1, i_tm2, i_tm3, &
+                                            io_c, i_h1, i_length ) &
+                                          BIND(C, name='stream_update_var_helmholtz_no_h2')
+      IMPORT :: C_DOUBLE, C_INT
+      REAL(KIND=C_DOUBLE), DIMENSION(*), INTENT(IN)    :: i_g1
+      REAL(KIND=C_DOUBLE), DIMENSION(*), INTENT(IN)    :: i_g2
+      REAL(KIND=C_DOUBLE), DIMENSION(*), INTENT(IN)    :: i_g3
+      REAL(KIND=C_DOUBLE), DIMENSION(*), INTENT(IN)    :: i_tm1
+      REAL(KIND=C_DOUBLE), DIMENSION(*), INTENT(IN)    :: i_tm2
+      REAL(KIND=C_DOUBLE), DIMENSION(*), INTENT(IN)    :: i_tm3
+      REAL(KIND=C_DOUBLE), DIMENSION(*), INTENT(INOUT) :: io_c
+      REAL(KIND=C_DOUBLE), DIMENSION(*), INTENT(IN)    :: i_h1
+      INTEGER(C_INT),      VALUE,        INTENT(IN)    :: i_length
+    END SUBROUTINE
+
     SUBROUTINE stream_vector_compscale( i_a, i_b, io_c, i_length ) &
                                             BIND(C, name='stream_vector_compscale')
       IMPORT :: C_DOUBLE, C_INT
